635fec48b568b24be778435163deab46746860cb
lukas spatschil <lukas@spatschil.at>
2020-07-16 14:10:31 +0200

    add licennce

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const app = require('./app');
-const port = process.argv.length >= 3 ? +process.argv[2] : 3000;
+const port = process.argv.length >= 3 ? + process.argv[2] : 3000;
 
 app.listen(port, () =>
     console.log(`Server is listening on http://localhost:${port}`)
