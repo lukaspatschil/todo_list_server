7e903506eb43b8e5f7fd859b63a2a9bc663afb3d
lukas spatschil <lukas@spatschil.at>
2020-07-16 11:06:21 +0200

    add post for todos and complete for todos

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const express = require('express');
+const { ValidationError } =require('express-validation');
 
 const todos = require('./routes/todos');
 
@@ -8,6 +9,14 @@ const app = express();
 app.use(express.json());
 
 app.use('/todo', todos);
+
+app.use(function(err, req, res, next) {
+    if (err instanceof ValidationError) {
+      return res.status(err.statusCode).json(err)
+    }
+   
+    return res.status(500).json(err)
+  })
 
 app.use((req, res, next) => {
     res.sendStatus(404);
--- routes/todos.js
+++ routes/todos.js
@@ -1,29 +1,68 @@
 'use strict';
 
 const express = require('express');
+const { validate, Joi } = require('express-validation');
+
 const routes = express.Router();
 
 const todos = [];
+const todoSchema = {
+    body: Joi.object({
+        name: Joi.string()
+            .required(),
+        description: Joi.string()
+    }),
+};
+
 todos.push({
-    title: 'New todo', 
+    id: 0,
+    name: 'New todo', 
     description: 'This is a new todo',
     completed: false
 });
 
+// get the todo
 routes.get('/', (req, res) => {
-    if (req.query.q !== null) {
+    if (req.query.q) {
         const index = Number(req.query.q);
         if (index < todos.length && index > -1) {
             res.send(todos[index]);
             return;
+        } else {
+            res.sendStatus(404);
         }
     }
 
     res.send(todos);
 });
 
-routes.post('/', (req, res) => {
-    // TODO: add validation for todo
+// create a new todo
+routes.post('/', validate(todoSchema, {}, {}), (req, res) => {
+    const todo = {
+        id: todos.length,
+        name: req.body.name,
+        description: req.body.description,
+        completed: false
+    };
+
+    todos.push(todo);
+    res.send(todo);
+});
+
+// complete the todo
+routes.post('/complete', (req, res) => {
+    if (!req.query.q) {
+        res.sendStatus(400);
+        return;
+    }
+    const index = Number(req.query.q);
+    if (index < 0 && index > todos.length - 1) {
+        res.sendStatus(404);
+        return;
+    }
+
+    todos[index].completed = !todos[index].completed;
+    res.send(todos[index]);
 })
 
 module.exports = routes;
